b9985ee626ca83d6199759c6382abc4faa2cd13d
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-10 01:59:31 -0600

    .

--- combat3/register/models.py
+++ combat3/register/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Player(models.Model):
     fullname = models.CharField(max_length=200)
-    email = models.EmailField()
     username = models.CharField(max_length=30, primary_key=True)
     password = models.CharField(max_length=50)
 
