21ec8c6061424d5730371841ad4ab2a58c5f586c
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-10 01:01:39 -0600

    added home view

--- combat3/homepage/views.py
+++ combat3/homepage/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request, user=""):
+    # do things pertaining to user later
+    return render(request, "homepage.html", {})
