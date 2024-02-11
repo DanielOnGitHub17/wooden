1630e96365e64ce1511497557d590a4c623c4881
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-11 15:11:38 -0600

    first

--- combat3/register/static/signs.js
+++ combat3/register/static/signs.js
@@ -1,6 +1,12 @@
 function handleMessage(){
     history.replaceState (1, "Normal Location", location.href.split('?')[0])
 }
+
+function removeFullname(){
+    if (location.href.includes("in")){
+        document.forms.querySelector("input").remove()
+    }
+}
 function doAll(){
     handleMessage()
 }
