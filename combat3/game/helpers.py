3b731d29f8599b302c5b8e904b5716542dcc5803
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-11 18:35:50 -0600

    changed normal number to range(number) to enable loopin

--- combat3/game/helpers.py
+++ combat3/game/helpers.py
@@ -1,10 +1,11 @@
 from random import randint
 
 
+# maze algorithm. 0, 1, 2. 0=space. 1=wood(breakable), 2=iron(protective)
 def make_game(dim):
     result = []
-    for i in dim:
-        result.append[[]]
-        for j in dim:
+    for i in range(dim):
+        result.append([])
+        for j in range(dim):
             result[-1].append(randint(0, 1))
     return result
