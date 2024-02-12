df5ef866b6c77857bc976a1d85977d57b61e66af
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-11 18:35:23 -0600

    added other static urls

--- combat3/combat3/settings.py
+++ combat3/combat3/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "register/static",
+    *(x+"/static" for x in ("register", "game")),
 ]
 
 LOGIN_URL = "/register/sign/in"
