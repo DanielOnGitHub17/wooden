b7f5e1e4f5e19f9fbdc490ae583811f93905af8b
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-11 21:34:58 -0600

    started Block.build. Started with adding block's div to world and adding specific classes and having the div reference the object

--- combat3/game/static/block.js
+++ combat3/game/static/block.js
@@ -1,6 +1,16 @@
 class Block{
-    constructor(){
-        3
-    }
+    constructor(kind){
+        // kind: 0 is blank, 1 is wood, 2 is iron
+        this.kind = kind;
+        Block.blocks[kind].push(this);
+        this.build();
+    };
+    
+    build(){
+        this.block = add(make(), game.world).className 
+             = `.block .${Block.backgrounds[this.kind]}`;
+        this.block.object = this;
+    };
     static blocks = [[], [], []];
+    static backgrounds = ["blank", "wood", "iron"];
 }
