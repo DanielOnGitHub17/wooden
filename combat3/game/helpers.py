630a9033cf33fc2a8d9162b7f14a34e3449acc2b
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-11 20:54:39 -0600

    I think I got there to some point. I might change the algorithm later

--- combat3/game/helpers.py
+++ combat3/game/helpers.py
@@ -3,8 +3,13 @@ from random import randint
 
 # maze algorithm. 0, 1, 2. 0=space. 1=wood(breakable), 2=iron(protective)
 def around(m, r, c):
-    (m[r-1][c], m[r+1][c], m[r][c-1], m[r][c+1])
-    return 
+    zero_length = 0
+    for x in (m[r-1][c], m[r+1][c], m[r][c-1], m[r][c+1]):
+        if not x:
+            zero_length += 1
+    return zero_length
+
+zero_based_choosing = [(0, 0), (0, 0), (0, 1), (0, 2), (1, 2)]
 
 def make_game(dim):
     # set result to up and down borders
@@ -17,6 +22,7 @@ def make_game(dim):
         for c in range(1, dim+1):
             #  self      up       down      left     right
             # [r][c]  [r-1][c]  [r+1][c]  [r][c-1]  [r][c+1]
-            neighbours = around(result, r, c)
+            # {4: (1, 2), 3: (0, 2), 2: (0, 1), 1, 0: (0)}
+            result[r][c] = randint(*zero_based_choosing[around(result, r, c)])
 
     return result
