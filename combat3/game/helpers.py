321453b9aef1229b8996a953907c10d3c2d7be04
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-11 20:41:51 -0600

    implementing make_game

--- combat3/game/helpers.py
+++ combat3/game/helpers.py
@@ -2,10 +2,21 @@ from random import randint
 
 
 # maze algorithm. 0, 1, 2. 0=space. 1=wood(breakable), 2=iron(protective)
+def around(m, r, c):
+    (m[r-1][c], m[r+1][c], m[r][c-1], m[r][c+1])
+    return 
+
 def make_game(dim):
-    result = []
-    for i in range(dim):
-        result.append([])
-        for j in range(dim):
-            result[-1].append(randint(0, 1))
+    # set result to up and down borders
+    result = [
+        [2] * (dim+2),
+        *[[2]+[randint(0, 2) for i in range(dim)]+[2] for i in range(dim)],
+        [2] * (dim+2),
+    ]
+    for r in range(1, dim+1):
+        for c in range(1, dim+1):
+            #  self      up       down      left     right
+            # [r][c]  [r-1][c]  [r+1][c]  [r][c-1]  [r][c+1]
+            neighbours = around(result, r, c)
+
     return result
