b7f69d73feada9f5ec98247367c8fa06cd8a8bff
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-12 22:59:03 -0600

    added code for draging an element

--- combat3/game/helpers.py
+++ combat3/game/helpers.py
@@ -9,22 +9,14 @@ def around(m, r, c):
             zero_length += 1
     return zero_length
 
-zero_based_choosing = [(0, 0), (0, 0), (0, 2), (0, 2), (0, 2)]
+irons = []
 
 def make_game(dim):
     # set result to up and down borders
-    result = [
+    return [
         [2] * (dim+2),
-        *[[2]+[randint(0, 2) for i in range(dim)]+[2] for i in range(dim)],
+        *[[2]+[
+            randint(0, 1) if i not in irons else 2 for i in range(dim)
+            ]+[2] for i in range(dim)],
         [2] * (dim+2),
     ]
-    with open("maze.txt", 'w') as maze:
-        maze.write('\n'.join(str(line) for line in result))
-    for r in range(1, dim+1):
-        for c in range(1, dim+1):
-            #  self      up       down      left     right
-            # [r][c]  [r-1][c]  [r+1][c]  [r][c-1]  [r][c+1]
-            # {4: (1, 2), 3: (0, 2), 2: (0, 1), 1, 0: (0)}
-            result[r][c] = randint(*zero_based_choosing[around(result, r, c)])
-
-    return result
--- combat3/static/funcs.js
+++ combat3/static/funcs.js
@@ -6,3 +6,16 @@ let get = (id) => document.getElementById(id)
   , getAll = (query) => document.querySelectorAll(query)
   , make = (tag='div') => document.createElement(tag)
   , add = (element, to=document.body) => to.appendChild(element);
+
+//   h.ondragstart = ()=>{
+//     h.x = event.screenX;
+//     h.y = event.screenY;
+// }
+// ()=>{
+//     h.x = event.screenX;
+//     h.y = event.screenY;
+// }
+// h.ondrag = ()=>{
+//     h.style.left = (event.screenX-h.x)+"px";
+//     h.style.top = (event.screenY-h.y)+"px";
+// }
