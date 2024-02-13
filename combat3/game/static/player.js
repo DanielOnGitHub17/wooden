5bf75f2a5e6ad19ff83a0728ffcdc520b012a423
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-12 20:50:56 -0600

    move algorithm... eventing for movement. do shooting later

--- combat3/game/static/player.js
+++ combat3/game/static/player.js
@@ -10,14 +10,28 @@ class Player{
     move(dir){
         // up: 0, right: 1, down: 2, left: 3
         // moves by navigating through blocks
-        let next = this.ground.position;
-        next[0] += moves[dir][0];
-        next[1] += moves[dir][1];
+        // given the direction. it adds
+        let next = [];
+        for (let i=0; i<2; i++){
+            next[i] = this.ground.position[i] + move[dir][i];
+        }
         let potentialGround = game.blocks.get(...next);
         if (!potentialGround){
             this.ground = potentialGround;
             this.ground.append(this.body);
         }
     }
-    static moves = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+    event(){
+        window.addEventListener("keyup", (event)=>{
+            if (event.key in Player.controls){
+                this.move(Player.controls.indexOf(event.key));
+            }
+        })
+    }
+    static moves = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+    static controls = ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"];
 }
+
+// for the other players, the movement updating should be by setInterval
+// getting the key of that database in the game to get it's position
