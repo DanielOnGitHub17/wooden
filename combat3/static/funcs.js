baf94c2d08bbf4f7af4b7eb1a486c55e10b68a98
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-12 22:35:30 -0600

    added a randInt function (maybe I should just commit with ... later)

--- combat3/static/funcs.js
+++ combat3/static/funcs.js
@@ -1,3 +1,6 @@
+// math
+let randInt = (x, y) => parseInt((y-x+1)*Math.random() + x);
+
 let get = (id) => document.getElementById(id)
   , getS = (query) => document.querySelector(query)
   , getAll = (query) => document.querySelectorAll(query)
