71eea6515f2d5bfd87dff35329ca918404d74400
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-02-12 20:50:29 -0600

    made game class better at referencing blocks

--- combat3/game/static/game.js
+++ combat3/game/static/game.js
@@ -4,12 +4,17 @@ class Game{
         this.gameRawMaterial = [...Game.world.children].map(i=>JSON.parse(i.textContent))
         Game.world.innerHTML = '';
         this.length = this.gameRawMaterial.length;
-        Game.world.style.width = Game.world.style.height = this.length*Block.dimension + 'px';
-        for (let i in this.gameRawMaterial){
-            this.gameRawMaterial[i].forEach(kind => {
-                new Block(kind);
+        // Game.world.style.width = Game.world.style.height = this.length*Block.dimension + 'px';
+        this.blocks = []
+        this.gameRawMaterial.forEach((textureLine, r)=>{
+            this.blocks.push([]);
+            textureLine.forEach((kind, c) => {
+                this.blocks[r].push(new Block(kind));
+                this.blocks[r][c].position = [r, c];
             });
-        }
+        });
+        this.blocks.get = (r, c) => this.blocks[r][c];
+        // Make another reference to blocks
     }
 
     start(){
