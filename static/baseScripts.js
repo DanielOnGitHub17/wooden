c310f18e2a7a2e06e9407c64d049cd4f88bc62d0
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-19 10:51:36 -0500

    no more showRegister

--- static/baseScripts.js
+++ static/baseScripts.js
@@ -1,13 +1,1 @@
 identify();
-addEventListener("click", (event)=>{
-    event.stopPropagation();
-    if (event.target.id == "showRegister") {
-        console.log("OUCH")
-        registerLinks.className = "visible";
-    }
-})
-
-
-addEventListener("click", (event)=>{
-    if (![showRegister, registerLinks].includes(event.target)) registerLinks.className = "";
-})
