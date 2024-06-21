da6e34fe0c0ad840164452cbdf366473438774fc
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-20 22:14:10 -0500

    better model

--- game/models.py
+++ game/models.py
@@ -1,24 +1,29 @@
-from django.db import models
-from django.contrib.auth.models import User
 import datetime
+
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+num_valid = lambda x, y: [MinValueValidator(x), MaxValueValidator(y)]
 
 # Create your models here.
 class Game(models.Model):
     size = models.IntegerField(default=15)
     initial_data = models.TextField(default="0")
     data = models.TextField(default="0") # will change by json.loadsing and dumpsing
-    max_hits = models.IntegerField(default=3)
-    n_real = models.IntegerField(default=2)
-    n_bots = models.IntegerField(default=2)
+    count = models.IntegerField(default=2, validators=num_valid(2, 7))
+    max_hits = models.IntegerField(default=3, validators=num_valid(2, 7))
     started_time = models.DateTimeField(default=datetime.time())
     ended_time = models.DateTimeField(default=datetime.time())
     started = models.BooleanField(default=False)
     ended = models.BooleanField(default=False)
     # primary key should be the link slash. (it will always change)
+    # do passcode later for private games - join with passcode...
+    # It will be so cool, the passcode becomes invalid when game starts
 
     @property
     def available(self):
-        return not (self.started or not self.ended)
+        return not (self.started or self.ended)
 
     @property
     def ongoing(self):
@@ -27,6 +32,10 @@ class Game(models.Model):
     @property
     def players(self):
         return Player.objects.filter(game=self)
+    
+    @property
+    def n(self):
+        return len(self.players)
 
     def __str__(self):
         return f"Game {self.pk}"
