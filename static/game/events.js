4fd1562eb94baab41c0d1afff561b6c8fce34b03
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-21 03:26:23 -0500

    Game maker. Switch screen to settings onload

--- static/game/events.js
+++ static/game/events.js
@@ -1,4 +1,13 @@
+addEventListener("submit", (event)=>{
+    event.preventDefault();
+    event.target == PRACTICE && (game = new Game(...new FormData(PRACTICE).values().map(i=>+i))).start();
+})
 
+
+onload = ()=>{
+    switchScreen("SETTINGS");
+    // switchScreen("GAME_OVER");
+}
 
 oncontextmenu=(event)=>{
     event.preventDefault()
