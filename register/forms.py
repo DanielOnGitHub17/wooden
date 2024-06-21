42b2daecc377a43526180ed4213ce4d41716d110
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-20 22:57:05 -0500

    username must be at least three characters - for game winners listing purposes

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,12 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        validators=[MinLengthValidator(3, 'Must be at least 3 characters')]
+    )
     class Meta:
         model = User
         fields = ["first_name", "last_name", "username", "password1", "password2"]
