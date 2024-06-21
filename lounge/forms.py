9ad1394b1cbf1aede80d1c792a5e9e6588a3cee8
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-20 22:15:42 -0500

    nothing here. CreateView does the Forming

--- lounge/forms.py
+++ lounge/forms.py
@@ -1,7 +1,2 @@
 from django.forms import ModelForm
 from game.models import Game
-
-class GameForm(ModelForm):
-    class Meta:
-        model = Game
-        fields = ["max_hits", "n_real", "n_bots"]
