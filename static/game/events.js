c7d5de025facc04c50776056e043808d9fb35656
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-21 21:09:43 -0500

    Firefox support. Fullscreen...

--- static/game/events.js
+++ static/game/events.js
@@ -1,11 +1,19 @@
 addEventListener("submit", (event)=>{
     event.preventDefault();
-    event.target == PRACTICE && (game = new Game(...new FormData(PRACTICE).values().map(i=>+i))).start();
+    if(event.target == PRACTICE) {
+        let prac = PRACTICE.elements;
+        game = new Game(+prac[0].value, +prac[1].value);
+        game.start();
+    }
 })
 
 
 onload = ()=>{
     switchScreen("SETTINGS");
+    if (Game.isMultiplayer) Gamer.load();
+    document.body.onclick = (event)=>{
+        event.target == document.body && document.body.requestFullscreen();
+    };
     // switchScreen("GAME_OVER");
 }
 
