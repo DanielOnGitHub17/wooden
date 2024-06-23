e3f31f4bde58512a8a9b1e8c44432b2bbafc0128
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-22 23:59:44 -0500

    events for socket communication. focus and blur set joined and present

--- static/game/events.js
+++ static/game/events.js
@@ -5,22 +5,62 @@ addEventListener("submit", (event)=>{
         game = new Game(+prac[0].value, +prac[1].value);
         game.start();
     }
-})
+});
 
+events = ()=>{
+    ["blur", "focus"].forEach((type, i)=>{
+        window.addEventListener(type, ()=>{
+            Gamer.user.present = i;
+            gameSocket.sendGamer(["present"]);
+        });
+    });
+}
+
+window.addEventListener("unload", (event)=>{
+    // Delete Gamer - no more in the game.
+    `
+    or maybe not. The user might have made a mistake -
+    `
+})
+window.addEventListener("beforeunload", (event)=>{
+    return false;
+});
 
 onload = ()=>{
     switchScreen("SETTINGS");
-    if (Game.isMultiplayer) Gamer.load();
-    document.body.onclick = (event)=>{
-        event.target == document.body && document.body.requestFullscreen();
-    };
+    loader();
+    if (Game.isMultiplayer) {
+        Gamer.load();
+        createGameSocket();
+        events();
+        return
+    }
     // switchScreen("GAME_OVER");
 }
 
-oncontextmenu=(event)=>{
-    event.preventDefault()
+function messageSocket (event){
+    // console.log(event, event.data);
+    let data = jsonObj(event.data);
+    gameSocket[data.handler](data.data);
+};
+
+function closeSocket (event) {
+    console.log("Game socket closed. Reforming connection...");
+    // create another one
+    createGameSocket();
+};
+
+function openSocket(event){
+    console.log("Connection established with channels");
+    // Tell everyone I am here...
+    gameSocket.sendGamer();
+};
+
+function errorSocket(event) {
+    event.preventDefault();
+    // event.target.close();
 }
 
-onbeforeunload=(event)=>{
-    return false
+oncontextmenu=(event)=>{
+    event.preventDefault();
 }
