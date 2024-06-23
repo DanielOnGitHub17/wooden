1672a3583995bf7ef3fbc4b2edbfeb8564b48a11
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-22 23:52:00 -0500

    player model from_username. Game try start initializes data. Might be the one to send it to the others...

--- game/models.py
+++ game/models.py
@@ -1,16 +1,20 @@
 import datetime
+import json
 
+from asgiref.sync import sync_to_async
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
+from helpers import group_send_sync, make_game
 
 num_valid = lambda x, y: [MinValueValidator(x), MaxValueValidator(y)]
 
 # Create your models here.
 class Game(models.Model):
     size = models.IntegerField(default=15)
-    initial_data = models.TextField(default="0")
-    data = models.TextField(default="0") # will change by json.loadsing and dumpsing
+    initial_data = models.TextField(default="")
+    data = models.TextField(default="") # will change by json.loadsing and dumpsing
     count = models.IntegerField(default=2, validators=num_valid(2, 7))
     max_hits = models.IntegerField(default=3, validators=num_valid(2, 7))
     started_time = models.DateTimeField(null=True)
@@ -21,6 +25,18 @@ class Game(models.Model):
     # do passcode later for private games - join with passcode...
     # It will be so cool, the passcode becomes invalid when game starts
 
+    def try_start(self):
+        if not self.started and self.joined == self.count:
+            game_data = make_game(
+                users=[player.user.username for player in self.players])
+            self.game = json.dumps(game_data)
+            self.started = True
+            game_data["time"] = datetime.now().timestamp() + 20
+            self.started_time = datetime.fromtimestamp(game_data["time"])
+            self.save()
+            return game_data
+            
+
     @property
     def available(self):
         return not (self.started or self.ended or self.n == self.count)
@@ -28,6 +44,10 @@ class Game(models.Model):
     @property
     def ongoing(self):
         return self.started and not self.ended
+
+    @property
+    def joined(self):
+        return sum(player.joined for player in self.players)
 
     @property
     def players(self):
@@ -62,13 +82,17 @@ class Player(models.Model):
     won = models.IntegerField(default=0)
     creator = models.BooleanField(default=False)
     # Socket needs
-    present = models.BooleanField(default=False)  # Paused (temporarily unavailable)/Resume
     joined = models.BooleanField(default=False)  # JOINED (checked once to see if joined)
+    present = models.BooleanField(default=False)  # Paused (temporarily unavailable)/Resume
     # For a user to join a game. The main thing should be to change the game attribute
     # will be given by difference between game.started and ended
     # (or game.started and logged out) Logout will have some work to do
     # it will have to check if the player was playing before he/she left
     # and other loopholes I will have to fill.
+
+    @staticmethod
+    def from_username(username):
+        return User.objects.get(username=username).player
 
     @property
     def rank(self):
