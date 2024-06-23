4be1d8e8b448fc8161a7aa43f96313629f4c6053
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-23 00:02:16 -0500

    socket event handlers

--- static/game/socket.js
+++ static/game/socket.js
@@ -1,5 +1,40 @@
-class WoodenSocket{
+function createGameSocket() {
+    delete gameSocket;  // Hopefully I won't be deleting the real gameSocket often
+    gameSocket = new GameSocket();
+    ["open", "close", "message", "error"].forEach(
+        event=>gameSocket.addEventListener(event, window[event+"Socket"])
+    );
+}
+
+class GameSocket extends WebSocket{
     constructor(){
-        //
+        super(`ws://${location.host}/ws/game/${Gamer.gameID}/`);
+    }
+    start(data){
+        for (let prop in data){
+            Game.rawMaterial[prop] = data[prop];
+        };
+        new Game();
+        game.start();
+    }
+    move(){}
+
+    createGamer(username){
+        // When someone joins the game but is still in their lounge page
+        new Gamer(username);
+    }
+
+    playerUpdate(data){
+        let player = Gamer.gamers[data.username];
+        for (let prop in data) player[prop] = data[prop];
+        if (data["joined"]){
+            COUNT.textContent = getAll("#PLAYERS_LIST td:nth-child(2)>input").filter(i=>i.checked).length
+        }
+    }
+
+    sendGamer(props=["joined", "present"]){
+        let player = Gamer.gamers[Gamer.username], obj = {};
+        ["username"].concat(props).forEach(prop => obj[prop] = player[prop]);
+        this.send(jsonStr({handler: "playerUpdate", "data": obj}));
     }
 }
