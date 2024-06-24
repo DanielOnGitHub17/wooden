c3938f157b3c0c64d9d89545418488e356646639
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-06-24 17:41:46 -0500

    createGameSocket wait till connected. start (game creation) bug fix. AND... move method (to send playerMove event). playerMove method to actually move a player (or bot) - WORKS!!! 5:21pm. I felt like crying.

--- static/game/socket.js
+++ static/game/socket.js
@@ -1,5 +1,8 @@
 function createGameSocket() {
     delete gameSocket;  // Hopefully I won't be deleting the real gameSocket often
+    if (window.gameSocket) {
+        while (gameSocket.CONNECTING) {}
+    }
     gameSocket = new GameSocket();
     ["open", "close", "message", "error"].forEach(
         event=>gameSocket.addEventListener(event, window[event+"Socket"])
@@ -10,14 +13,25 @@ class GameSocket extends WebSocket{
     constructor(){
         super(`ws://${location.host}/ws/game/${Gamer.gameID}/`);
     }
+
     start(data){
+        Game.player = Gamer.username
         for (let prop in data){
             Game.rawMaterial[prop] = data[prop];
         };
-        new Game();
+        window.game = new Game(Object.values(data.positions).length, data.hits);
         game.start();
     }
-    move(){}
+
+    move(dir){
+        this.send(jsonStr({
+            handler: "playerMove", data: {username: Gamer.username, dir: dir}
+        }))
+    }
+
+    playerMove(data){
+        Gamer.gamers[data.username].player.move(data.dir);
+    }
 
     createGamer(username){
         // When someone joins the game but is still in their lounge page
@@ -35,6 +49,6 @@ class GameSocket extends WebSocket{
     sendGamer(props=["joined", "present"]){
         let player = Gamer.gamers[Gamer.username], obj = {};
         ["username"].concat(props).forEach(prop => obj[prop] = player[prop]);
-        this.send(jsonStr({handler: "playerUpdate", "data": obj}));
+        this.send(jsonStr({handler: "playerUpdate", data: obj}));
     }
 }
