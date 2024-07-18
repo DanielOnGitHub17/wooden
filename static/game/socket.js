5ffb9ee735a2aa732f4c8870f1a605e86ebaef0e
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-07-18 12:59:50 -0500

    support both http and https

--- static/game/socket.js
+++ static/game/socket.js
@@ -11,7 +11,7 @@ function createGameSocket() {
 
 class GameSocket extends WebSocket{
     constructor(){
-        super(`wss://${location.host}/ws/game/${Gamer.gameID}/`);
+        super(`ws${'s'.repeat(location.protocol=="https:")}://${location.host}/ws/game/${Gamer.gameID}/`);
     }
 
     start(data){
