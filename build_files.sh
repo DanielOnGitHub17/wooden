96d883004c2f43eb69b8e0a9ea46e0ba4ab3df26
Daniel Enesi <enesidaniel.120064@gmail.com>
2024-07-20 10:52:28 -0500

    Start Daphne myself?

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,4 @@ pip install -r requirements.txt
 python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
+python3.9 -m daphne wooden.asgi:app # Imagine if this works :)
