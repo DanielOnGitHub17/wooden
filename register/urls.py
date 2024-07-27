d039bd9536e107e05f39d898359a033e8e48f9ff
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-07-27 18:46:35 +0000

    Added paths for password reset support. Removed unused imports.

--- register/urls.py
+++ register/urls.py
@@ -1,13 +1,13 @@
-from django.contrib import admin
-from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from register import views
 
-# to make things easier, I might include django.contrib.auth.urls
 urlpatterns = [
     path("signup/", views.SignUp.as_view(), name="Sign up"),
     path("activate/<str:uidb64>/<str:token>/", views.activate, name="Activate"),
     path("signin/", views.SignIn.as_view(), name="Log in"),
     path("signout/", views.SignOut.as_view(), name="Log out"),
+    path("password_reset/", views.ResetPassword.as_view(), name="Reset password"),
+    path("password_reset_done/", views.DoneResetPassword.as_view(), name="password_reset_done"),
+    path("password_reset_confirm/<str:uidb64>/<str:token>/", views.ConfirmResetPassword.as_view(), name="password_reset_confirm"),
 ]
