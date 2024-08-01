bcf1cb80708bca5108d85cfed95dcd75ae653b4f
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-08-01 04:07:23 +0000

    The consumer for chat. Receives, processes/validates (nothing for now), broadcasts. From, on, and to "chat". Simple.

---
+++ chat/consumers.py
@@ -0,0 +1,27 @@
+import json
+import time
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add("chat", self.channel_name)
+        await self.accept()
+    
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        # Check data message [1], if it is bad, change it to "[BLOCKED]"
+        # Maybe block [0] if it is bad.
+        # Broadcast chat to all.
+        await self.channel_layer.group_send(
+            "chat", {"type": "chat.message", "data": json.dumps(data)}
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=event["data"])
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            "chat", self.channel_name
+        )
+
