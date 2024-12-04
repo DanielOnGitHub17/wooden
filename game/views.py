190f493baa839f8039c0da7201b8695ca2e04537
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-04 01:35:31 +0000

    Add docstrings for game views and improve code readability

--- game/views.py
+++ game/views.py
@@ -1,24 +1,22 @@
-import json
-import os
+"""Views for the game app."""
 
 from django.contrib import messages as msg
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.views import View
-from django.views.generic.edit import UpdateView
 
-from game.models import Game, Player
-from helpers import group_send_sync, make_game, WoodenError
+from game.models import Game
+from helpers import group_send_sync, make_game
 
 
 class LeaveGame(LoginRequiredMixin, View):
+    """View for leaving a game."""
     def post(self, request):
+        """Leave a game."""
         player = request.user.player
         game = player.game
-        message, next = "You left the game successfully", "/play/"
+        message, next_ = "You left the game successfully", "/play/"
         if not game:
             message = "Error occured (NIG)"
         elif game.ongoing:
@@ -28,15 +26,18 @@ class LeaveGame(LoginRequiredMixin, View):
         else:
             message = "Sorry you could not wait! Make a game/join one below."
             player.reset(end=player.creator)
-            next = "/lounge/"
+            next_ = "/lounge/"
             # Tell other players.
-            group_send_sync(group_name=game.id, data={"handler": "playerLeave", "data": request.user.username})
+            group_send_sync(group_name=game.id, data={
+                "handler": "playerLeave", "data": request.user.username})
 
         msg.add_message(request, msg.ERROR, message)
-        return redirect(next)
+        return redirect(next_)
 
 class StartEarly(LoginRequiredMixin, View):
+    """View for starting a game early."""
     def post(self, request):
+        """Start a game early."""
         player = request.user.player
         game = player.game
         if not game or game.ongoing or game.no_of_players < 2:
@@ -49,6 +50,7 @@ class StartEarly(LoginRequiredMixin, View):
 
 @login_required
 def play(request):
+    """View for playing a game."""
     player = request.user.player
     game = player.game
     error_msg = (not game) * "You are not in a game.\n"
@@ -68,14 +70,17 @@ def play(request):
         player.save()
     context = {
         "multiplayer": True,
-        "players": {player.user.username: [player.joined, player.present] for player in game.players},
+        "players": {player.user.username: [
+            player.joined, player.present] for player in game.players},
         "game": game,
         "game_data": game.try_start()
     }
     return render(request, "app/game.html", context)
-    
+
 class JoinGame(LoginRequiredMixin, View):
+    """View for joining a game."""
     def post(self, request):
+        """Join a game."""
         player = request.user.player
         if player.game:
             msg.add_message(request, msg.WARNING, "You are already in a game")
@@ -89,13 +94,17 @@ class JoinGame(LoginRequiredMixin, View):
                 msg.add_message(request, msg.SUCCESS, "You joined the game successfully")
                 # Send the "Gamer's" username, with event "createGamer"
                 # Channel to all (Message will create an object on all player's platforms with joined=false, present=false)
-                group_send_sync(group_name=game_id, data={"handler": "createGamer", "data": request.user.username})
+                group_send_sync(group_name=game_id, data={
+                    "handler": "createGamer", "data": request.user.username})
         except:
-            msg.add_message(request, msg.ERROR, "An error occured so you could not join the game")
+            msg.add_message(request, msg.ERROR
+                            , "An error occured so you could not join the game")
         return redirect("/play/")
 
 class EndGame(LoginRequiredMixin, View):
+    """View for ending a game."""
     def post(self, request):
+        """End a game."""
         won = +("won" in request.POST)
         request.user.player.reset(won)
         msg.add_message(request, msg.SUCCESS
@@ -105,4 +114,5 @@ class EndGame(LoginRequiredMixin, View):
 
 # @login_required
 def practice(request):
+    """View for practicing a game."""
     return render(request, "app/game.html", {"game_data": make_game(10)})
