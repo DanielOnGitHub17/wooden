b70e7e65ffa0e35e13d3fc299e31fb488cac02eb
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-04 00:00:08 +0000

    Refactor Lounge view to improve readability and add module docstring
    removed unnecessary imports

--- lounge/views.py
+++ lounge/views.py
@@ -1,40 +1,38 @@
-import json
+"""This module contains the view for the lounge page."""
+# import json
 
 from django.contrib import messages as msg
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.core.exceptions import ValidationError
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
-from django.views import View
 from django.views.generic.edit import CreateView
 
-from game.models import Game, Player
-from helpers import make_game, online_players_context
-from random import randint, sample, choice
+from game.models import Game
+from helpers import online_players_context
 
 
 class Lounge(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    """View for the lounge page."""
     template_name = "app/lounge.html"
     model = Game
     fields = ["no_of_players", "max_hits"]
 
     def form_valid(self, form):
-        player = self.request.user.player 
-        if not player.game:
-            player.game = form.save()
-            player.creator = True
-            player.save()
-            msg.add_message(self.request, msg.SUCCESS, "You created and joined the game successfully")
-            return super().form_valid(form)
+        player = self.request.user.player
+        if player.game:
+            return None
+        player.game = form.save()
+        player.creator = True
+        player.save()
+        msg.add_message(self.request, msg.SUCCESS
+                        , "You created and joined the game successfully")
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(online_players_context())
         if not self.request.user.player.game:
             context.update({
-                "games": [game for game in Game.objects.all() if game.available]
+                "games": [game for game in Game.objects.all() if game.available]  # type: ignore
             })
         return context
 
