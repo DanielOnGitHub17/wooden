485a50c5fbbc81901bc2a745ad3bac68b6557a38
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-04 01:30:55 +0000

    Add custom forms for user registration and login with enhanced docstrings

--- register/forms.py
+++ register/forms.py
@@ -1,9 +1,11 @@
+"""Script to create forms for user registration and login"""
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 
 class EmailField(forms.EmailField):
+    """Custom EmailField to check if email is already in use."""
     def validate(self, value):
         super().validate(value)
         if User.objects.filter(email=value).exists():
@@ -11,6 +13,7 @@ class EmailField(forms.EmailField):
         return value
 
 class SignUpForm(UserCreationForm):
+    """Form for user registration."""
     username = forms.CharField(
         validators=[MinLengthValidator(3, 'Must be at least 3 characters')]
     )
@@ -18,10 +21,13 @@ class SignUpForm(UserCreationForm):
     last_name = forms.CharField(max_length=30, required=True)
     email = EmailField(required=True, label="Email Address")
     class Meta:
+        """Meta class for SignUpForm."""
         model = User
         fields = ["email", "first_name", "last_name", "username", "password1", "password2"]
 
 class SignInForm(AuthenticationForm):
+    """Form for user login."""
     class Meta:
+        """Meta class for SignInForm."""
         model = User
         fields = ["username", "password"]
