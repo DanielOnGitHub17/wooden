050d047652b4825458fca1c27ae89fe5f3383b09
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-04 04:05:53 -0600

    Remove STATICFILES_DIRS configuration for simplified static file management
    Fix warning on heroku that 'staticfiles' dir DNE

--- wooden/settings.py
+++ wooden/settings.py
@@ -178,9 +178,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
 
 # Simplified static file serving.
 # https://whitenoise.readthedocs.io/en/stable/django.html#django
