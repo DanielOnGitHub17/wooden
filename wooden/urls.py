f9cd43e99794c066347e6e0175b7412e607413ea
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-04 15:50:06 -0600

    Update admin URL configuration to use environment variable for security
    removed unneccessary imports

--- wooden/urls.py
+++ wooden/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
+import os
+
 from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(os.getenv("DJANGO_ADMIN_URL"), admin.site.urls),
     path("", include("homepage.urls"), name="General"),
     path("", include("register.urls"), name="Registration"),
     path("", include("chat.urls"), name="Chats"),
