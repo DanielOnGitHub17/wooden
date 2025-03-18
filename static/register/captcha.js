4095c3a090466a37c1b1eac8146c3038d4335109
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-03-18 05:31:16 +0000

    Add reCAPTCHA validation to registration forms and update class names for consistency. Add styles to show recaptcha validation errors

---
+++ static/register/captcha.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = getS(".register-form");
+    const recaptchaErrors = get("recaptcha-errors");
+
+    form.addEventListener("submit", function(event) {
+        if (!grecaptcha.getResponse()) {
+            event.preventDefault();
+            recaptchaErrors.textContent = "Please complete the reCAPTCHA.";
+        }
+    });
+});
