1079cdf3d3dc0cb8404957b40cb3d7df38597a2a
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2026-03-05 20:21:54 +0000

    Make quick sign in the main URL

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="Home"),
+    path("about/", views.home, name="Home"),
     path("game_help/", views.game_help, name="Game Help"),
     path("dev/", views.dev, name="Developers"),
     path("support/", views.Support.as_view(), name="Support"),
