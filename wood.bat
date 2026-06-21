fbde2af63a6c750767ff62272bf9521ae21e4988
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-06-21 01:39:10 -0700

    Serve windows with daphne. use windows convention

--- wood.bat
+++ wood.bat
@@ -7,77 +7,77 @@ set "command=%1"
 
 if "!command!"=="" (
     echo No command specified. Available commands:
-    echo   -a    Activate virtual environment
-    echo   -c    Check Django configuration
-    echo   -cd   Check Django configuration for deployment
-    echo   -d    Run Redis in Docker
-    echo   -m    Run migrations
-    echo   -mm   Make migrations
-    echo   -R    Run Gunicorn server with reload
-    echo   -r    Run Django development server
-    echo   -s    Run Django shell
-    echo   -st   Collect static files
-    echo   -t    Run tests
-    echo   -req  Install requirements
+    echo   /a    Activate virtual environment
+    echo   /c    Check Django configuration
+    echo   /cd   Check Django configuration for deployment
+    echo   /d    Run Redis in Docker
+    echo   /m    Run migrations
+    echo   /mm   Make migrations
+    echo   /R    Run Gunicorn server with reload
+    echo   /r    Run Django daphne development server
+    echo   /s    Run Django shell
+    echo   /st   Collect static files
+    echo   /t    Run tests
+    echo   /req  Install requirements
     exit /b 1
 )
 
-if "!command!"=="-a" (
+if "!command!"=="/a" (
     call ..\woodenv\Scripts\activate.bat
     exit /b 0
 )
 
-if "!command!"=="-c" (
+if "!command!"=="/c" (
     python manage.py check
     exit /b 0
 )
 
-if "!command!"=="-cd" (
-    python manage.py check --deploy
+if "!command!"=="/cd" (
+    python manage.py check -/deploy
     exit /b 0
 )
 
-if "!command!"=="-d" (
-    docker run --rm -p 6379:6379 redis:latest
+if "!command!"=="/d" (
+    docker run --rm /p 6379:6379 redis:latest
     exit /b 0
 )
 
-if "!command!"=="-m" (
+if "!command!"=="/m" (
     python manage.py migrate
     exit /b 0
 )
 
-if "!command!"=="-mm" (
+if "!command!"=="/mm" (
     python manage.py makemigrations
     exit /b 0
 )
 
-if "!command!"=="-R" (
+if "!command!"=="/R" (
     python -m gunicorn --reload --log-level debug wooden.asgi:application -k uvicorn.workers.UvicornWorker
     exit /b 0
 )
 
-if "!command!"=="-r" (
-    python manage.py runserver 5006
+if "!command!"=="/r" (
+    python -m daphne -b 0.0.0.0 -p 5006 wooden.asgi:application
     exit /b 0
 )
 
-if "!command!"=="-s" (
+if "!command!"=="/s" (
     python manage.py shell
     exit /b 0
 )
 
-if "!command!"=="-st" (
+if "!command!"=="/st" (
     python manage.py collectstatic --noinput
     exit /b 0
 )
 
-if "!command!"=="-t" (
+if "!command!"=="/t" (
     python manage.py test
     exit /b 0
 )
 
-if "!command!"=="-req" (
+if "!command!"=="/req" (
     python -m pip install -r requirements.txt --require-virtualenv
     exit /b 0
 )
